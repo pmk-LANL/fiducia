79b47a6a4fd8ef49a2e001a2ef308c2b26418e8e
pkozlowski <pkozlowski@lanl.gov>
2021-03-19 16:14:03 -0600

    Added example data fetcher to correctly get absolute paths of files on any system.

--- fiducia/data/__init__.py
+++ fiducia/data/__init__.py
@@ -7,3 +7,29 @@ Submodule/directory with data for examples and testing.
 
 @author: Pawel M. Kozlowski
 """
+
+import os.path as osp
+
+
+def _fetch(data_filename):
+    r"""
+    Fetches the absolute path of the file give the relative location
+    of the file within the fiducia package.
+    
+    Parameters
+    ----------
+    data_filename : str
+        Relative path of file within the fiducia.data submodule.
+        
+    Returns
+    -------
+    resolved_path : 
+        Absolute path to data file within fiducia.data submodule.
+    """
+    data_dir = osp.abspath(osp.dirname(__file__))
+    resolved_path = osp.join(data_dir, '..', data_filename)
+    # check that the resolved path corresponds to a files that exists
+    if not osp.isfile(resolved_path):
+        raise Exception(f"No such file {resolved_path}")
+    return resolved_path
+
