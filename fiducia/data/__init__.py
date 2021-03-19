a4d24030199bf93ece3c97011f6c1e02bd1a7412
pkozlowski <pkozlowski@lanl.gov>
2021-03-19 16:28:08 -0600

    Added functions for fetching example data on any system.

--- fiducia/data/__init__.py
+++ fiducia/data/__init__.py
@@ -33,3 +33,36 @@ def _fetch(data_filename):
         raise Exception(f"No such file {resolved_path}")
     return resolved_path
 
+
+def shot89336():
+    r"""
+    Gets absolute paths to example data for shot 89336 on Omega-60.
+    """
+    # raw Dante data file 
+    danteFileRel = 'data/dante_dante89336.dat'
+    # corresponding response functions file
+    responseFileRel = 'data/do171127_2018-10-29_response_functions.csv'
+    # file of input uncertainties for each channel
+    responseUncertaintyFileRel = 'data/exampleuncertainty_1d.csv'
+    # oscilloscope offsets file for calibrating channel signals
+    offsetsFileRel = 'data/Offset.xls'
+    # oscilloscope attenuators file to correct for addition attenuation
+    # on individual channels. Note: that the identification numbers of the
+    # attenuators used for an Omega-60 shot should be recorded in the header
+    # of the raw Dante data file.
+    attenuatorsFileRel = 'data/TableAttenuators.xls'
+    
+    # getting absolute paths to data files
+    danteFileAbs = _fetch(danteFileRel)
+    responseFileAbs = _fetch(responseFileRel)
+    responseUncertaintyFileAbs = _fetch(responseUncertaintyFileRel)
+    offsetsFileAbs = _fetch(offsetsFileRel)
+    attenuatorsFileAbs = _fetch(attenuatorsFileRel)
+    
+    # saving absolute paths into a dict
+    dataDict = {'Raw Dante' : danteFileAbs,
+                'Response Funcs' : responseFileAbs,
+                'Response Uncertainty' : responseUncertaintyFileAbs,
+                'Oscilloscope Offsets' : offsetsFileAbs,
+                'Attenuators' : attenuatorsFileAbs}
+    return dataDict
